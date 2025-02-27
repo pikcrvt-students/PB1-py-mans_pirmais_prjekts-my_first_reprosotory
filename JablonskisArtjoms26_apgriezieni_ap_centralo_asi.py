57feb3bd71191d4591c6b5d4345dbbee42bf91fb
Artjoms Jablonskis <A240526AJ@rvt.lv>
2025-02-27 09:05:06 +0200

    Izvadītas, 4 grupas no 8, bez animacijas.

--- JablonskisArtjoms26_apgriezieni_ap_centralo_asi.py
+++ JablonskisArtjoms26_apgriezieni_ap_centralo_asi.py
@@ -2,19 +2,22 @@
 """
     ASCII animacijas. Burtu rotacija ap centralo asi.
 """
+from time import sleep
+
 print("R")
 print(" O")
-print("   T")
-
+print("   T", flush=True)
+sleep(1)
 
 print(" R")
 print(" O")
-print(" T")
-
+print(" T", flush=True)
+sleep(1)
 
 print("  R")
 print(" O")
-print("T")
+print("T", flush=True)
+sleep(1)
 
-print("T O R")
-
+print("T O R", flush=True)
+sleep(1)
