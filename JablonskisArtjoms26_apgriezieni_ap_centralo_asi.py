c2117931d71f7c267431c1b84ba928db6c554340
Artjoms Jablonskis <A240526AJ@rvt.lv>
2025-02-27 08:56:32 +0200

    Izvadītas, 4 grupas no 8, bez animacijas.

---
+++ JablonskisArtjoms26_apgriezieni_ap_centralo_asi.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+    ASCII animacijas. Burtu rotacija ap centralo asi.
+"""
+print("R")
+print(" O")
+print("   T")
+
+
+print(" R")
+print(" O")
+print(" T")
+
+
+print("  R")
+print(" O")
+print("T")
+
+print("T O R")
+
