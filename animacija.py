1c6622ac421f8f926544563af479ce0eb9a38cd9
Artjoms Jablonskis <A240526AJ@rvt.lv>
2025-03-03 09:37:13 +0200

    Es izveidoju divas ASCII animacijas

---
+++ animacija.py
@@ -0,0 +1,104 @@
+from time import sleep
+import os
+
+cilveks= '''
+   _
+  (_)
+  /|\\
+ / | \\
+   |  
+  / \\       _
+ /   \\     (_)'''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+    _
+   (_)
+   /|\\
+  / | \\
+    |  
+    |\\      _
+    | \\    (_)'''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+     _
+    (_)
+    /|\\
+   / | \\
+     |  
+     |    _
+     |   (_)'''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+      _
+     (_)
+     /|\\
+    / | \\
+      |  
+     /|   _
+    / |  (_)'''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+       _
+      (_)
+      /|\\
+     / | \\
+       |  
+      / \\  _
+     /   \\(_)'''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+       _
+      (_)
+      /|\\
+     / | \\
+       |      _
+      / \\    (_)
+     /   \\   '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+       _
+      (_)
+      /|\\
+     / | \\        _
+       |         (_)
+      / \\    
+     /   \\   '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+       _
+      (_)
+      /|\\
+     / | \\       
+       |            _
+      / \\          (_)
+     /   \\   '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+       _
+      (_)
+      /|\\
+     / | \\       
+       |             
+      / \\              _
+     /   \\            (_)'''
+print(cilveks)
+sleep(1)
+os.system('cls')
+
+
---
+++ otra_animacija.py
@@ -0,0 +1,87 @@
+from time import sleep
+import os
+
+cilveks= '''
+
+   _
+  (_)
+  /|\\
+ /0| \\0
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+
+   _
+  (_) 0
+  /|\\
+ / |0\\
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+   0
+   _
+  (_) 
+  /|\\
+ / | \\0
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+   
+0  _
+  (_) 0
+  /|\\
+ / | \\
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+   0
+   _
+  (_) 
+  /|\\
+0/ | \\
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+   
+0  _
+  (_) 
+  /|\\
+ / |0\\
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
+cilveks= '''
+   
+   _
+  (_) 
+  /|\\
+ /0| \\0
+   |  
+  / \\
+ /   \\ '''
+print(cilveks)
+sleep(1)
+os.system('cls')
